b3a21268f3d17ef6dc27aeb941e1f2accedd7956
Aaron Levi Can <aaronlevican@gmail.com>
2025-05-23 00:03:47 +0200

    feat: added functions to create/destroy a client socket

--- include/triac/socket.h
+++ include/triac/socket.h
@@ -1,36 +1,49 @@
 #ifndef TRIAC_SOCKET_H
 #define TRIAC_SOCKET_H
 
-// typing stuff
 #include <stdint.h>
 #include <stdbool.h>
 #include <sys/types.h>
 
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include <sys/socket.h> // socket(), bind(), accept()
+#include <arpa/inet.h> // sockaddr_in, htons, etc.
+#include <netinet/in.h> // inet_pton(), inet_ntoa()
+#include <unistd.h> // close()
 
-// struct that holds a socket information
-// it is used to crate a new socket
-typedef struct triac_socket triac_socket_t;
-
+typedef struct triac_socket_server triac_socket_server_t;
+typedef struct triac_socket_client triac_socket_client_t;
 
 /**
  * @brief Creates a new server socket
  *
  * Opens a TCP socket on given port and configures it
  *
- * @param sock Pointer to the socket struct
+ * @param socket Pointer to the socket struct
  * @param port Port that should be used. Should be between 0 and 65535
  * @param non_blocking Non-blocking or blocking
- * @return 0 on success, -1 on error
+ * @return triac_socket_server
  */
-int triac_socket_create(uint16_t port, bool non_blocking);
+triac_socket_server_t* triac_socket_server_create(uint16_t port, bool non_blocking);
 
+/**
+ * @brief Creates a new client socket and configure it
+ *
+ * Connects via TCP to given port
+ *
+ * @param socket Pointer to the socket struct
+ * @param port Port to connect to. Should be between 0 and 65535
+ * @return triac_socket_client
+ */
+triac_socket_client_t* triac_socket_client_create(uint16_t port, const char* address);
 
-// triack_socket_accept
-// triac_socket_listen
-// triac_socket_close
+/**
+ * @brief Destroys client socket
+ *
+ * Destroys client socket and frees up memory
+ *
+ * @param tsc Pointer to the socket struct
+ * @return triac_socket_client
+ */
+void triac_socket_client_destroy(triac_socket_client_t *tsc);
 
 #endif
--- src/socket.c
+++ src/socket.c
@@ -1,16 +1,73 @@
-#include <triac/socket.h>
-
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
+#include "triac/socket.h"
 
-struct triac_socket {
+struct triac_socket_server {
   int fd;
   uint16_t port;
   bool non_blocking;
 };
 
+struct triac_socket_client {
+  int fd;
+  struct sockaddr_in server_address;
+};
 
-int create_server() {
+triac_socket_server_t* triac_socket_server_create(uint16_t port, bool non_blocking) {
   return 0;
+}
+
+triac_socket_client_t* triac_socket_client_create(uint16_t port, const char* address) {
+
+  // create new tsc struct
+  triac_socket_client_t* tsc = malloc(sizeof(triac_socket_client_t));
+  if (!tsc) {
+    perror("[triac] malloc() failed");
+    return NULL;
+  }
+
+  // the socket() function creates a new socket and returns the index pointing at the socket in the socket table of the operating system
+  // the function takes the following arguments: TCP or UDP (AF_INET/2 for TCP), SOCK_STREAM (bc TCP), 0 for protocol default
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock < 0) {
+    perror("[triac] socket() failed");
+    free(tsc);
+    return NULL;
+  }
+  tsc->fd = sock;
+
+  // now we configure the socket using the sockaddr_in struct from in.h
+  // the struct needs 3 members: sin_family, sin_port, sin_addr
+  tsc->server_address.sin_port = port;
+  tsc->server_address.sin_family = AF_INET;
+  tsc->server_address.sin_port = htons(port); // we use htons() to convert port to the right data format (host to network short)
+  // we use inet_pton() (presentation to numeric), to convert the char* to inet_addr_t and directly write it to the struct
+  int result = inet_pton(AF_INET, address, &tsc->server_address.sin_addr);
+  if (result <= 0) {
+    if (result == 0) {
+      fprintf(stderr, "[triac] Invalid address given to inet_pton()\n");
+    }
+    else {
+      perror("[triac] inet_pton() failed");
+    }
+    close(tsc->fd);
+    free(tsc);
+    return NULL;
+  }
+
+  // nothing failed, yay!
+  printf("[triac] Client socket created\n");
+  return tsc;
+}
+
+void triac_socket_client_destroy(triac_socket_client_t *tsc) {
+  if (!tsc) return;
+  if (tsc->fd >= 0) {
+    close(tsc->fd);
+  }
+  free(tsc);
+  printf("[triac] Client socket closed\n");
 }
