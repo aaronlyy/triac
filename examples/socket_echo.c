2699f9455a8af2452bc699b43c37f3410550bc0b
Aaron Levi Can <aaronlevican@gmail.com>
2025-05-23 00:03:19 +0200

    chore: removed file

--- examples/socket_echo.c
+++
@@ -1,9 +0,0 @@
-// how to create a simple socket server with triac
-#include <triac/socket.h>
-
-#include <stdio.h>
-
-int main() {
-  int code = create_server();
-  printf("Das ist der Code: %d", code);
-}
