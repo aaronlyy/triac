2bc89ac019d2d5176c85a629f59ac96699518d0b
Aaron Levi Can <aaronlevican@gmail.com>
2025-05-24 22:59:59 +0200

    chore: added new example

--- examples/socket_client.c
+++ examples/socket_client.c
@@ -1,11 +1,20 @@
 // example how to create a socket client and connect to a server
 #include <stdio.h>
+
 #include "triac/socket.h"
+#include "triac/constants.h"
 
 int main() {
-  triac_socket_client_t *tsc = triac_socket_client_create(3402, "0.0.0.0");
 
-  printf("wow!\n");
+  triac_socket_t* client = triac_socket_alloc();
 
-  triac_socket_client_destroy(tsc);
+  triac_socket_set_address_family(client, TRIAC_S_AF_UNSPEC);
+  triac_socket_set_host(client, "0.0.0.0");
+  triac_socket_set_port(client, "3402");
+  triac_socket_set_options(client, TRIAC_S_OPT_REUSEADDR);
+  
+  triac_socket_create(client);
+  triac_socket_connect(client);
+
+  triac_socket_destroy(client);
 }
