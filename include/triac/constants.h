d4ed6cb9ecdc7f36fe88905c1d479c95f0452b01
Aaron Levi Can <aaronlevican@gmail.com>
2025-05-24 19:29:43 +0200

    feat: added constants for socket options to be used as bitmasks and more

---
+++ include/triac/constants.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <sys/socket.h>
+
+// socket related constants
+#define TRIAC_AF_IPV4 AF_INET // IPv4 address family
+#define TRIAC_AF_IPV6 AF_INET6 // IPv6 address family
+
+#define TRIAC_PF_IPV4 PF_INET // IPv4 address family
+#define TRIAC_PF_IPV6 PF_INET6 // IPv6 address family
+
+#define TRIAC_TCP SOCK_STREAM // TCP Socket
+#define TRIAC_UDP SOCK_DGRAM // UPD Socket
+
+#define TRIAC_S_OPT_NONBLOCK (1 << 0)
+#define TRIAC_S_OPT_REUSEADDR (1 << 1)
+#define TRIAC_S_OPT_KEEPALIVE (1 << 2)
+#define TRIAC_S_OPT_TCP_NODELAY (1 << 3)
+#define TRIAC_S_OPT_TIMEOUT (1 << 4)
+#define TRIAC_S_OPT_REUSEPORT (1 << 5)
